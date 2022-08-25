f3d5b056fed280b66b0b1eacf4e65fb2858572b5
Viktor Edman <viktor@edman.se>
2022-08-25 11:57:59 +0200

    added stateful logic to bro page

--- pages/bro.js
+++ pages/bro.js
@@ -1,5 +1,18 @@
-import Bro from '../components/Bro'
+import Bro from "../components/Bro";
+import { useState } from "react";
 
-export default function bro() {
-    return <Bro/>
+export default function BroPage() {
+  const [bro, setBro] = useState(1)
+  const broArray = []
+  for (let index = 0; index < bro; index++) {
+    broArray.push(<Bro key={index}/>)
+    
+  }
+  return (
+    <>
+        <button onClick={() => setBro(bro - 1)}>-</button>
+        <button onClick={() => setBro(bro + 1)}>+</button>
+        <div>{broArray.map(bro => bro)}</div>
+    </>
+  );
 }
